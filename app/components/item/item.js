eaf57a55d03dfae605029eb555ab66825d180a3f
Lindy Crain <lindylcrain@gmail.com>
2016-09-07 22:38:11 -0600

    stying changes

--- app/components/item/item.js
+++ app/components/item/item.js
@@ -28,10 +28,3 @@
     }
 
 }())
-
-// <button style="display: inline-block; font-size: 10px" ng-click="$ctrl.moveTask($ctrl.taskObj)">Move Task</button>
-
-// <button class='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Move To</button>
-//                 <ul class='dropdown-menu' ng-repeat="list in $ctrl.lists">
-//                   <li ng-click='$ctrl.moveTask($ctrl.taskObj)'>{{$ctrl.newList.name}}</li>
-//                 </ul>
